c0ac592123545f5fb9e21872daa62d773dfadb45
SergioC <SergioC@192.168.4.243>
2016-09-28 17:02:47 -0300

    Commit inicial

---
+++ application/views/inicio_view.php
@@ -0,0 +1,52 @@
+﻿<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Control de Stock</title>
+        
+        <!-- BOOTSTRAP STYLES-->
+        <link href="<?php echo base_url('assets/css/bootstrap.css'); ?>" rel="stylesheet" />
+        
+        <!-- FONT-AWESOME STYLES-->
+        <link href="<?php echo base_url('assets/css/font-awesome.min.css'); ?>" rel="stylesheet" type="text/css"/>
+        
+        <!-- CUSTOM STYLES-->
+        <link href="<?php echo base_url('assets/css/custom.css'); ?>" rel="stylesheet" />
+        
+        <!-- GOOGLE FONTS-->
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+
+    </head>
+    <body>
+
+        <?php include 'navbar.php'; ?>
+
+
+
+
+
+
+
+
+
+
+
+
+        <?php include 'footer.php'; ?>
+
+        <!-- /. WRAPPER  -->
+        <!-- SCRIPTS -AT THE BOTOM TO REDUCE THE LOAD TIME-->
+        
+        <!-- JQUERY SCRIPTS -->
+        <script src="<?php echo base_url('assets/js/jquery-1.10.2.js'); ?>"></script>
+        
+        <!-- BOOTSTRAP SCRIPTS -->
+        <script src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
+        
+        <!-- CUSTOM SCRIPTS -->
+        <script src="<?php echo base_url('assets/js/custom.js'); ?>"></script>
+
+
+    </body>
+</html>
---
+++ application/views/navbar.php
@@ -0,0 +1,64 @@
+<div id="wrapper">
+    <div class="navbar navbar-inverse navbar-fixed-top">
+        <div class="adjust-nav">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+                <a class="navbar-brand" href="#">
+                    <img src="assets/img/logo.png" width="90" height="60"/>
+                </a>
+            </div>
+
+            <span class="logout-spn" >
+                <a href="#" style="color:#fff;">SALIR</a>  
+
+            </span>
+        </div>
+    </div>
+    
+    
+    <!-- /. NAV TOP  -->
+    <nav class="navbar-default navbar-side" role="navigation">
+        <div class="sidebar-collapse navbar-collapse">
+            <ul class="nav" id="side-menu">
+
+
+                <li>
+                    <a href="#" ><i class="fa fa-truck "></i> Proveedores</a>
+                </li>
+
+
+                <li>
+                    <a href="#"><i class="fa fa-wrench "></i> Insumos</a>
+                </li>
+
+                <li>
+                    <a href="#"><i class="fa fa-shopping-cart"></i> Compras</a>
+                </li>
+
+
+            </ul>
+        </div>
+
+    </nav>
+    <!-- /. NAV SIDE  -->
+    <div id="page-wrapper" >
+        <div id="page-inner">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2>BLANK PAGE </h2>   
+                </div>
+            </div>              
+            <!-- /. ROW  -->
+            <hr />
+
+            <!-- /. ROW  -->           
+        </div>
+        <!-- /. PAGE INNER  -->
+    </div>
+    <!-- /. PAGE WRAPPER  -->
+</div>
