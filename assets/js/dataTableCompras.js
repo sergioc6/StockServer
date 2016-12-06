ca541fccfcd7b97e0789db2fea10cf34c239ff00
Nahuel Leiva <educmack@gmail.com>
2016-12-06 17:45:37 -0300

    - Cambios varios

---
+++ assets/js/dataTableCompras.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('#compras').DataTable({
+        "language": {
+            "lengthMenu": "Mostrar _MENU_ compras por pág.",
+            "zeroRecords": "Ninguno encontrado",
+            "info": "Mostrando pág _PAGE_ de _PAGES_",
+            "infoEmpty": "No hay órdenes de compra",
+            "infoFiltered": "(filtrado de un total de _MAX_ compras)"
+        }
+    } );
+    
+    
+});
+
+
